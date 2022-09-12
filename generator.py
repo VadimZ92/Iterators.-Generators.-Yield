8cf0024bb284e3cdc2399ffd0f127f341cab5403
VadimZ92 <hemulyaa@gmail.com>
2022-09-12 22:45:29 +0500

    update

--- generator.py
+++ generator.py
@@ -6,23 +6,23 @@ nested_list = [
 
 
 def flat_generator(nested_list):
-    value = 0
-    len(nested_list)
-    while value != len(nested_list):
-        yield nested_list[value]
-        value += 1
-
-
-if __name__ == '__main__':
-    for item in flat_generator(nested_list):
+    for item in nested_list:
         for var in item:
             if type(var) == list:
                 for var1 in var:
                     if type(var1) != list:
-                        print(var1)
+                        yield var1
                     else:
                         for var2 in var1:
                             if type(var2) != list:
-                                print(var2)
+                                yield var2
             else:
-                print(var)
+                yield var
+
+
+if __name__ == '__main__':
+    for item in flat_generator(nested_list):
+        print(item)
+    print()
+    flat_list = [item for item in flat_generator(nested_list)]
+    print(flat_list)
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-nested_list = [
+main_list = [
     ['a', 'b', 'c'],
     ['d', 'e', 'f', 'h', False],
     [1, 2, None],
@@ -7,34 +7,29 @@ nested_list = [
 
 class FlatIterator:
 
-    def __init__(self, value):
-        self.value = -1
+    def __init__(self, some_list):
+        self.main_list = some_list
 
     def __iter__(self):
-        # self.nested_list = nested_list
+        self.main_list_cursor = 0  # курсор основного списка
+        self.nested_list_cursor = -1  # курсор списка вложенного в основной список
         return self
 
     def __next__(self):
-        self.value += 1
-        if self.value == len(nested_list):
-            raise StopIteration
-        return nested_list[self.value]
+
+        while len(main_list) != self.main_list_cursor:
+            self.nested_list_cursor += 1
+            if len(main_list[self.main_list_cursor]) == self.nested_list_cursor:
+                self.main_list_cursor += 1
+                self.nested_list_cursor = 0
+            if len(main_list) == self.main_list_cursor:
+                raise StopIteration
+            return self.main_list[self.main_list_cursor][self.nested_list_cursor]
 
 
 if __name__ == '__main__':
-    for item in FlatIterator(nested_list):
-        for var in item:
-            if type(var) == list:
-                for var1 in var:
-                    if type(var1) != list:
-                        print(var1)
-                    else:
-                        for var2 in var1:
-                            if type(var2) != list:
-                                print(var2)
-            else:
-                print(var)
-
+    for item in FlatIterator(main_list):
+        print(item)
 print()
-flat_list = [var for item in FlatIterator(nested_list) for var in item]
+flat_list = [item for item in FlatIterator(main_list)]
 print(flat_list)
